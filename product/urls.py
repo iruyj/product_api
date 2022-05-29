1d85fdd498a05d014a3003810c8d245c021e92c9
Jyuri <jeon1310@gmail.com>
2022-05-30 03:30:11 +0900

    url 재정리

--- product/urls.py
+++ product/urls.py
@@ -8,7 +8,6 @@ app_name = 'product'
 urlpatterns = [
     # path('list/<str:keyword>', views.getlist, name="list"),
     path('', views.index, name="index"),
-    path('info/', views.getProduct, name="info"),
     path('filter/<int:type>', views.filtering, name="filter"),
     path('<int:code>/', views.detail, name="detail"),
 ]
